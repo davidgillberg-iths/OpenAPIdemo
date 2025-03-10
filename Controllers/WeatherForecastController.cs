a8a9cfc2e086a64c878aefece4ae545167b62e71
DavidAtMollimers <david.gillberg@iths.se>
2025-03-10 12:42:02 +0100

    Add xml comments to the docs for WeatherController

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace OpenAPIdemo.Controllers
 {
+    /// <summary>
+    /// Hanterar väderprognoser.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -13,12 +16,22 @@ namespace OpenAPIdemo.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+        /// <summary>
+        /// Skapar en ny instans av <see cref="WeatherForecastController"/>.
+        /// </summary>
+        /// <param name="logger">Logger för att hantera loggning.</param>
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Hämtar en lista med väderprognoser för de kommande dagarna.
+        /// </summary>
+        /// <returns>En lista med <see cref="WeatherForecast"/>-objekt.</returns>
+        /// <response code="200">Returnerar en lista med väderprognoser.</response>
         [HttpGet(Name = "GetWeatherForecast")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen(options =>
 {
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+
     options.SwaggerDoc("v1", new OpenApiInfo
     {
         Title = "Mitt API",
         Version = "v1",
-        Description = "Ett exempel på ett ASP.NET Core API med anpassad Swagger-dokumentation",
+        Description = "Ett exempel pï¿½ ett ASP.NET Core API med anpassad Swagger-dokumentation",
         Contact = new OpenApiContact
         {
             Name = "David Gillberg",
@@ -28,6 +28,11 @@ builder.Services.AddSwaggerGen(options =>
         }
     });
 });
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
 
 
 var app = builder.Build();
